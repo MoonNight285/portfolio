0e9a6f59bd9c0c4c51858811d842c0d617173bfb
MoonNight285 <65889750+MoonNight285@users.noreply.github.com>
2023-02-17 00:25:37 +0900

    slick 캐러셀 여러 화면에서도 UI 적절하게 보이게 적용

--- src/body/SkillContents.jsx
+++ src/body/SkillContents.jsx
@@ -4,17 +4,35 @@ import "./SkillContents.css";
 import "./slick-theme.css";
 import "./slick.css";
 
-// slick 캐러셀 옵션 설정
-const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 1
-};
-
 function SkillContents({titleName, imgFolderPath, imgList}) {
     const [imgUrl, setImgUrl] = useState([]);
+    const [sildeShowCount, setSildeShowCount] = useState(4);
+    const [width, setWidth] = useState(0);
+
+    // slick 캐러셀 옵션 설정
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: sildeShowCount,
+        slidesToScroll: 1
+    };
+
+    useEffect(() => {
+        setWidth(window.screen.width);
+    }, []);
+
+    useEffect(() => {
+        if (width > 1200) {
+            setSildeShowCount(4);
+        } else if (width > 990) {
+            setSildeShowCount(3);
+        } else if (width >= 768) {
+            setSildeShowCount(2);
+        } else {
+            setSildeShowCount(1);
+        }
+    }, [width]);
 
     useEffect(() => {
         let tempUrl = [];
@@ -48,6 +66,7 @@ function SkillContents({titleName, imgFolderPath, imgList}) {
                         }
                     </div>
                     :
+                    width > 988 ?
                     <div className={"div-skill-no-slick"}>
                         {
                             imgUrl.map(item => {
@@ -59,6 +78,22 @@ function SkillContents({titleName, imgFolderPath, imgList}) {
                             })
                         }
                     </div>
+                        :
+                        <div className={"div-slider-wrapper"}>
+                            {
+                                <Slider {...settings}>
+                                    {
+                                        imgUrl.map(item => {
+                                            return (
+                                                <div key={item.key} className={"div-skill-logo-wrapper"}>
+                                                    <img className={"img-skill-logo"} key={item.key} src={item.url} />
+                                                </div>
+                                            );
+                                        })
+                                    }
+                                </Slider>
+                            }
+                        </div>
             }
         </div>
     );
