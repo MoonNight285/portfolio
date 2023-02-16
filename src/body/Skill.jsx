a8b561520502b1e21bf7ef91227d16abf7473b0e
MoonNight285 <65889750+MoonNight285@users.noreply.github.com>
2023-02-16 23:51:24 +0900

    slick 캐러셀 추가 노트북 환경까지 UI 적용함

--- src/body/Skill.jsx
+++ src/body/Skill.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import SkillContents from "./SkillContents";
 
+const frontImgList = ["html.png", "css.png", "JavaScript-Logo.png", "jquery.png",
+    "bootstrap-logo-shadow.png", "react.png"];
+const backImgList = ["java.png", "jsp-logo.png", "mysql-tutorial.png",
+    "springboot-1.webp", "thymeleaf.png", "cShapp.png", "mssql.png"];
+const collaborationImgList = ["git.png", "SourceTree.png", "notion.png"];
+
 function Skill() {
     return (
         <div className={"container"}>
             <div className={"row"}>
                 <div className={"col"}>
-                    <SkillContents titleName={"Frontend"} />
+                    <SkillContents titleName={"Frontend"} imgFolderPath={"front"} imgList={frontImgList} />
                 </div>
             </div>
             <div className={"row"}>
                 <div className={"col"}>
-                    <SkillContents titleName={"Backend"} />
+                    <SkillContents titleName={"Backend"} imgFolderPath={"back"} imgList={backImgList} />
                 </div>
             </div>
             <div className={"row"}>
                 <div className={"col"}>
-                    <SkillContents titleName={"collaboration"} />
+                    <SkillContents titleName={"collaboration"} imgFolderPath={"collaboration"} imgList={collaborationImgList} />
                 </div>
             </div>
         </div>
--- src/body/SkillContents.jsx
+++ src/body/SkillContents.jsx
@@ -1,10 +1,65 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import Slider from "react-slick";
 import "./SkillContents.css";
+import "./slick-theme.css";
+import "./slick.css";
 
-function SkillContents({titleName}) {
+// slick 캐러셀 옵션 설정
+const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1
+};
+
+function SkillContents({titleName, imgFolderPath, imgList}) {
+    const [imgUrl, setImgUrl] = useState([]);
+
+    useEffect(() => {
+        let tempUrl = [];
+        for (let i = 0; i < imgList.length; ++i) {
+            tempUrl.push({
+                key : i,
+                url : `/images/${imgFolderPath}/${imgList[i]}`
+            });
+        }
+        setImgUrl(tempUrl);
+    }, []);
+
     return (
         <div id={"div-skill-contents"}>
-            <h5>{titleName}</h5>
+            <h5 className={"h5-skill-title"}>{titleName}</h5>
+            {
+                imgList.length >=5 ?
+                    <div className={"div-slider-wrapper"}>
+                        {
+                            <Slider {...settings}>
+                                {
+                                    imgUrl.map(item => {
+                                        return (
+                                            <div key={item.key} className={"div-skill-logo-wrapper"}>
+                                                <img className={"img-skill-logo"} key={item.key} src={item.url} />
+                                            </div>
+                                        );
+                                    })
+                                }
+                            </Slider>
+                        }
+                    </div>
+                    :
+                    <div className={"div-skill-no-slick"}>
+                        {
+                            imgUrl.map(item => {
+                                return (
+                                    <div key={item.key} className={"div-skill-logo-wrapper"}>
+                                        <img className={"img-skill-logo"} key={item.key} src={item.url} />
+                                    </div>
+                                );
+                            })
+                        }
+                    </div>
+            }
         </div>
     );
 }
