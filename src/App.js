4a4a23c17cf7ae815c7cf7f578cbf73256dd7841
MoonNight285 <65889750+MoonNight285@users.noreply.github.com>
2023-02-28 14:44:53 +0900

    포트폴리오 부분 이미지 표시 및 설명 추가

--- src/App.js
+++ src/App.js
@@ -6,6 +6,72 @@ import Skill from "./body/Skill";
 import PortfolioContent from "./body/PortfolioContent";
 import Footer from "./footer/Footer";
 
+const covid19ImgList = [
+    "/images/covid19/covid19_1.png",
+    "/images/covid19/covid19_2.png",
+    "/images/covid19/covid19_3.png",
+    "/images/covid19/covid19_4.png",
+    "/images/covid19/covid19_5.png",
+    "/images/covid19/covid19_6.png"
+    ];
+
+const covid19ImgContents = [
+    "도시와 날짜를 선택해 모든 데이터를 보여줍니다.",
+    "도시와 날짜를 선택해서 누적 확진자 수를 보여줍니다.",
+    "공지사항 목록을 표시합니다.",
+    "관리자 로그인 기능입니다.",
+    "공지사항 작성 기능입니다.",
+    "현재 내 계정 상태 확인 및 관리자 신청 대기목록을 볼수있습니다."
+]
+
+const cinemaImgList = [
+    "/images/cinema/cinema_1.png",
+    "/images/cinema/cinema_2.png",
+    "/images/cinema/cinema_3.png",
+    "/images/cinema/cinema_4.png",
+    "/images/cinema/cinema_5.png",
+    "/images/cinema/cinema_6.png",
+    "/images/cinema/cinema_7.png",
+    "/images/cinema/cinema_8.png"
+];
+
+const cinemaImgContents = [
+    "스프링 시네마의 메인화면입니다.",
+    "DB에 현재 등록되어있는 현재상영작을 보여줍니다.",
+    "DB에 저장된 데이터를 기반으로 영화의 상세정보를 표시합니다.",
+    "약관동의 화면입니다.",
+    "회원가입 페이지입니다.",
+    "현재 상영중인 영화목록에서 영화를 선택하는 페이지입니다.",
+    "좌석 선택페이지입니다.",
+    "결제페이지입니다."
+]
+
+const dreamcomImgList = [
+    "/images/dreamcom/dreamcom_1.png",
+    "/images/dreamcom/dreamcom_2.png",
+    "/images/dreamcom/dreamcom_3.png",
+    "/images/dreamcom/dreamcom_4.png",
+    "/images/dreamcom/dreamcom_5.png",
+    "/images/dreamcom/dreamcom_6.png",
+    "/images/dreamcom/dreamcom_7.png",
+    "/images/dreamcom/dreamcom_8.png",
+    "/images/dreamcom/dreamcom_9.png",
+    "/images/dreamcom/dreamcom_10.png"
+];
+
+const dreamcomImgContents = [
+    "드림컴퓨터의 메인화면입니다.",
+    "메인화면의 하단부분으로 스크롤시 새롭게 아이템을 불러옵니다.",
+    "검색결과를 보여주는 페이지입니다.",
+    "제품 상세페이지입니다.",
+    "제품 상세페이지중 구매후기를 표시하는 부분입니다.",
+    "주문내역과 배송조회를 확인하는 페이지입니다.",
+    "주문결제하기전 확인하는 페이지입니다.",
+    "아임포트 결제모듈을 사용한 결제화면입니다.",
+    "회원가입 페이지입니다.",
+    "이메일과 비밀번호찾기 페이지입니다."
+]
+
 function App() {
   return (
     <div id={"div-main-app"}>
@@ -13,7 +79,9 @@ function App() {
         <Introduction />
         <AboutMe />
         <Skill />
-        <PortfolioContent />
+        <PortfolioContent portfolioName={"드림컴"} imgList={dreamcomImgList} imgContentList={dreamcomImgContents} />
+        <PortfolioContent portfolioName={"스프링 시네마"} imgList={cinemaImgList} imgContentList={cinemaImgContents} />
+        <PortfolioContent portfolioName={"코로나 모니터"} imgList={covid19ImgList} imgContentList={covid19ImgContents} />
         <Footer />
     </div>
   );
--- src/body/PortfolioContent.jsx
+++ src/body/PortfolioContent.jsx
@@ -1,37 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import "./PortfolioContent.css";
 import {Carousel} from "react-bootstrap";
 
-function PortfolioContent({portfolioName}) {
+function PortfolioContent({portfolioName, imgList, imgContentList}) {
+    const [viewImgExplanationNumber, setViewImgExplanationNumber] = useState(0); // 이미지에 맞춰 표시할 이미지 내용
+
     return (
         <div className={"container"}>
             <div className={"row"}>
                 <div className={"col d-flex justify-content-center"}>
                     <div id={"div-portfolio-box"}>
-                        <Carousel>
-                            <Carousel.Item>
-                                <img className="d-block w-100" src="holder.js/800x400?text=First slide&bg=373940"
-                                    alt="First slide"/>
-                                <Carousel.Caption>
-                                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                                </Carousel.Caption>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <img className="d-block w-100" src="holder.js/800x400?text=Second slide&bg=282c34"
-                                    alt="Second slide"/>
-                                <Carousel.Caption>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                </Carousel.Caption>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <img className="d-block w-100" src="holder.js/800x400?text=Third slide&bg=20232a"
-                                    alt="Third slide"/>
-                                <Carousel.Caption>
-                                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                                </Carousel.Caption>
-                            </Carousel.Item>
+                        <h3 id={"h3-portfolio-name"}>{portfolioName}</h3>
+                        <Carousel indicators={false} onSlid={(number) => {setViewImgExplanationNumber(number)}}>
+                            {
+                                imgList.map(item => {
+                                    return (
+                                        <Carousel.Item key={item}>
+                                            <img className="d-block w-100" src={item}
+                                                 alt={imgContentList[viewImgExplanationNumber]}/>
+                                        </Carousel.Item>
+                                    );
+                                })
+                            }
                         </Carousel>
-                        <h3 id={"h3-portfolio-name"}>프로젝트 이름</h3>
+                        <div className={"div-portfolio-img-explanation"}>{imgContentList[viewImgExplanationNumber]}</div>
                     </div>
                 </div>
             </div>
